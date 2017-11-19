72686c6688550613888c19fe47e83ab677c6d683
jiro.kirameki <deziyama0404@gmail.com>
2017-11-19 02:50:14 +0000

    user login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,7 @@ class UsersController < ApplicationController
   end
 
   def show
+    @user = User.find(params[:id])
   end
 
   def new
@@ -13,7 +14,7 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
-    
+
     if @user.save
       flash[:success] = 'ユーザを登録しました。'
       redirect_to @user
@@ -28,4 +29,6 @@ class UsersController < ApplicationController
   
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
 end
+
--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,5 @@ Rails.application.routes.draw do
  delete 'logout', to: 'sessions#destroy'
  
  get 'signup', to: 'users#new'
- resource :users, only: [:index, :show, :new, :create]
+ resources :users, only: [:index, :show, :new, :create]
 end
